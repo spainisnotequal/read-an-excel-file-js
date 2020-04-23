228e55b5d7d1e678f5c9c6b398058878632169b2
spainisnotequal <spainisnotequal@gmail.com>
2020-04-23 17:56:52 +0200

    Get the field names of each sheet

--- index.js
+++ index.js
@@ -7,29 +7,17 @@ const XLSX = require("xlsx");
 const fooWorkbook = XLSX.readFile("./foo.xlsx");
 
 // log every row in every sheet of the workook
-fooWorkbook.SheetNames.map((sheet) => {
-  console.log("Name of the sheet:", sheet);
-  XLSX.utils
-    .sheet_to_json(fooWorkbook.Sheets[sheet])
-    .map((row) => console.log(row));
+fooWorkbook.SheetNames.map((sheetName) => {
+  console.log("Name of the sheet:", sheetName);
+
+  const sheet = XLSX.utils.sheet_to_json(fooWorkbook.Sheets[sheetName]);
+
+  if (sheet[0]) console.log("Fields of the sheet:", Object.keys(sheet[0]));
+  else console.log("The sheet is empty.");
+
+  // Log each row of the sheet
+  sheet.map((row) => console.log(row));
 });
-
-// Process each sheet individually
-const fooSheetNameList = fooWorkbook.SheetNames; // ordered list of the sheets in the fooWorkbook
-
-//first sheet
-const dataFirstFooSheet = XLSX.utils.sheet_to_json(
-  fooWorkbook.Sheets[fooSheetNameList[0]]
-);
-console.log("Data from the first sheet");
-dataFirstFooSheet.map((row) => console.log(row));
-
-// second sheet
-const dataSecondFooSheet = XLSX.utils.sheet_to_json(
-  fooWorkbook.Sheets[fooSheetNameList[1]]
-);
-console.log("Data from the second sheet");
-dataSecondFooSheet.map((row) => console.log(row));
 
 /* ------------------------------ */
 /* Second woorkbook (xls version) */
@@ -38,26 +26,14 @@ dataSecondFooSheet.map((row) => console.log(row));
 const barWorkbook = XLSX.readFile("./bar.xls");
 
 // log every row in every sheet of the workook
-barWorkbook.SheetNames.map((sheet) => {
-  console.log("Name of the sheet:", sheet);
-  XLSX.utils
-    .sheet_to_json(barWorkbook.Sheets[sheet])
-    .map((row) => console.log(row));
+barWorkbook.SheetNames.map((sheetName) => {
+  console.log("Name of the sheet:", sheetName);
+
+  const sheet = XLSX.utils.sheet_to_json(barWorkbook.Sheets[sheetName]);
+
+  if (sheet[0]) console.log("Fields of the sheet:", Object.keys(sheet[0]));
+  else console.log("The sheet is empty.");
+
+  // Log each row of the sheet
+  sheet.map((row) => console.log(row));
 });
-
-// Process each sheet individually
-const barSheetNameList = barWorkbook.SheetNames; // ordered list of the sheets in the barWorkbook
-
-//first sheet
-const dataFirstBarSheet = XLSX.utils.sheet_to_json(
-  barWorkbook.Sheets[barSheetNameList[0]]
-);
-console.log("Data from the first sheet");
-dataFirstBarSheet.map((row) => console.log(row));
-
-// second sheet
-const dataSecondBarSheet = XLSX.utils.sheet_to_json(
-  barWorkbook.Sheets[barSheetNameList[1]]
-);
-console.log("Data from the second sheet");
-dataSecondBarSheet.map((row) => console.log(row));
