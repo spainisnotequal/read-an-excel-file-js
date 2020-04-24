f30eb5355d96488ab55b54fcbf7281fe06c01d04
spainisnotequal <spainisnotequal@gmail.com>
2020-04-24 10:43:56 +0200

    Log cell info if the cell is not empty, or a message otherwise

--- index.js
+++ index.js
@@ -22,21 +22,27 @@ const fields = Object.keys(sheetData[0]);
 console.log(fields);
 
 // get the data in A2 (is a Date field)
-console.log("type:", sheet.A2.t); //=> "d" means date
-console.log("raw value:", sheet.A2.v);
-console.log("formatted text:", sheet.A2.w);
-console.log("Excel format:", sheet.A2.z);
+if (sheet.A2) {
+  console.log("type:", sheet.A2.t); //=> "d" means date
+  console.log("raw value:", sheet.A2.v);
+  console.log("formatted text:", sheet.A2.w);
+  console.log("Excel format:", sheet.A2.z);
+} else console.log("Empy cell");
 
 // get the data in B2 (is a Concept field)
-console.log("type:", sheet.B2.t); //=> "s" means text
-console.log("raw value:", sheet.B2.v);
-console.log("formatted text:", sheet.B2.w);
-console.log("Excel format:", sheet.B2.z);
+if (sheet.B2) {
+  console.log("type:", sheet.B2.t); //=> "s" means text
+  console.log("raw value:", sheet.B2.v);
+  console.log("formatted text:", sheet.B2.w);
+  console.log("Excel format:", sheet.B2.z);
+} else console.log("Empy cell");
 
 // get the data in C2 (is an Amount field)
-console.log("type:", sheet.C2.t); //=> "n" means number
-console.log("raw value:", sheet.C2.v);
-console.log("formatted text:", sheet.C2.w);
-console.log("Excel format:", sheet.C2.z);
+if (sheet.C2) {
+  console.log("type:", sheet.C2.t); //=> "n" means number
+  console.log("raw value:", sheet.C2.v);
+  console.log("formatted text:", sheet.C2.w);
+  console.log("Excel format:", sheet.C2.z);
+} else console.log("Empy cell");
 
 //
